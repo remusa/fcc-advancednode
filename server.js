e72e91d5dd570fb8f27a282bb8d6479240232897
R Sánchez <rene.as.himself@gmail.com>
2019-01-31 08:57:03 -0600

    03 Set up Passport

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fccTesting = require('./freeCodeCamp/fcctesting.js')
 
+const session = require('express-session')
+const passport = require('passport')
+
 const app = express()
 
 app.set('view engine', 'pug')
@@ -12,6 +15,16 @@ fccTesting(app) //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || 'secret',
+        resave: true,
+        saveUninitialized: true,
+    })
+)
+
+app.use(passport.initialize())
 
 app.route('/').get((req, res) => {
     res.render(process.cwd() + '/views/pug/index', {
