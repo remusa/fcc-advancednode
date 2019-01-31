e342c0711dc0aae3259a6d70cea1fa07460ecb2c
R Sánchez <rene.as.himself@gmail.com>
2019-01-31 08:35:00 -0600

    01 Set up a Template Engine

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js')
 
 const app = express()
 
+app.set('view engine', 'pug')
+
 fccTesting(app) //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.route('/').get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html')
+    res.render('pug/index.pug')
 })
 
 app.listen(process.env.PORT || 3000, () => {
